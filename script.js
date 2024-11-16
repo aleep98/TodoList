7e01805b0d86b5f55c84eb085308170f3435eb6f
Alexandre.P <127621361+aleep98@users.noreply.github.com>
2024-11-16 12:43:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 let listElement = document.querySelector("#app");
+let inputElement = document.querySelector("#app input");
+let buttonElement = document.querySelector("#app button");
+
+let tarefas = [];
+
+function adicionarTarefa (){
+    if(inputElement.value === ''){
+        alert ("Digite alguma tarefa");
+        return false;
+    }else{
+        let novaTarefa = inputElement.value;
+
+        tarefas.push(novaTarefa);
+        inputElement.value = '';
+    }
+}
+
+buttonElement.onclick = adicionarTarefa;
